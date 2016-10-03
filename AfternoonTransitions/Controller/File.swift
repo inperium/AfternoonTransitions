1cea9b65969121558f823759f3628c6bdb1bf3e1
inperium <ibow9416@740s-pl-111.local>
2016-10-03 12:02:22 -0600

    Linked storyboard.

---
+++ AfternoonTransitions/Controller/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  AfternoonTransitions
+//
+//  Created by Bowen, Isaac on 10/3/16.
+//  Copyright © 2016 Bowen, Isaac. All rights reserved.
+//
+
+import Foundation
---
+++ AfternoonTransitions/Controller/UpdatedViewController.swift
@@ -0,0 +1,14 @@
+//
+//  UpdatedViewController.swift
+//  AfternoonTransitions
+//
+//  Created by Bowen, Isaac on 10/3/16.
+//  Copyright © 2016 Bowen, Isaac. All rights reserved.
+//
+
+import UIKit
+
+class UpdatedViewController : UIViewController
+{
+    
+}
